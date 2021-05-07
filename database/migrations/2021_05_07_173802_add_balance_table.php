d3e9014e1c960cad8d7d799e9f66e6c8d410a3b2
Vladislav Kryshtanovskiy <wladislav.k@gmail.com>
2021-05-07 13:43:19 -0400

    Add entries migration

---
+++ database/migrations/2021_05_07_173802_add_balance_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBalanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->decimal('value');
+            $table->timestamp('date')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
