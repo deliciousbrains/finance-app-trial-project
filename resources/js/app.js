b5ac985bde26246272068d5bed329ea79fd7abc5
Ben Meek <ben.meek@immediate.co.uk>
2021-06-07 01:20:05 +0100

    phase 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,11 @@ require("alpinejs");
 window.DateTime = require("luxon").DateTime;
 window.VueCookies = require("vue-cookies");
 
-// window.Vue = require("vue").default;
-
-// Register Vue Components
-// Vue.component("dashboard-component", require("./components/DashboardComponent.vue").default);
-
 import Vue from "vue";
 import App from "./vue/app";
 
 Vue.use(VueCookies);
 
-// Initialize Vue
 const app = new Vue({
     el: "#app",
     components: {
